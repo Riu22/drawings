fbb1d281fb09ac2b6f1ac58885ff39749824337b
Riu <riutordjaume@gmail.com>
2025-10-21 23:25:09 +0200

    todo funciona como debe

--- src/main/java/com/drawing/servelt/controller/firstjsp.java
+++ src/main/java/com/drawing/servelt/controller/firstjsp.java
@@ -6,14 +6,14 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.io.PrintWriter;
 
-@WebServlet(value = "/firs")
 
-public class first_controller extends HttpServlet {
+@WebServlet(value = "/")
+public class firstjsp extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        req.getRequestDispatcher("WEB-INF/index.jsp")
+
+        req.getRequestDispatcher("/WEB-INF/jsp/first.jsp")
                 .forward(req, resp);
     }
 }
